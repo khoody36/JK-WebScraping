52546ba2930c50c8cfe5150e3e3169b3e7839881
Joey <jisola@nevada.unr.edu>
2022-03-21 15:43:23 -0700

    recommit

--- Tommy B.R
+++ Tommy B.R
@@ -14,7 +14,7 @@ paths_allowed("https://www.nfl.com/players/tom-brady/stats/career")
 link <- "https://www.nfl.com/players/tom-brady/stats/career"
 
 #Pull stats 
-TBstats <- read_html(link) %>% html_nodes(".nfl-t-stats__col-16") %>% html_text2()
+TBstats <- read_html(link) %>% html_nodes(".nfl-t-stats__col-16") %>% html_text()
 
 #Make stats by year
 StatsCat <- as.data.frame(TBstats[c(1:17)])
@@ -43,16 +43,16 @@ TBstats2021 <- as.data.frame(t(TBstats[c(375:391)]))
 
 #Categories
 categories <- c('YEAR','Team','G','ATT','COMP','PCT',
-                 'YDS','AVG','LNG','TD','INT','1st','1st%',
-                 '20+','SCK','SCKY','RATE')
+                'YDS','AVG','LNG','TD','INT','1st','1st%',
+                '20+','SCK','SCKY','RATE')
 
 #List of data frames
 ListTBdf <- list(TBstats2000, TBstats2001, TBstats2002, TBstats2003,
-               TBstats2004, TBstats2005, TBstats2006, TBstats2007,
-               TBstats2008, TBstats2009, TBstats2010, TBstats2011,
-               TBstats2012, TBstats2013, TBstats2014, TBstats2015,
-               TBstats2016, TBstats2017, TBstats2018, TBstats2019,
-               TBstats2020, TBstats2021)
+                 TBstats2004, TBstats2005, TBstats2006, TBstats2007,
+                 TBstats2008, TBstats2009, TBstats2010, TBstats2011,
+                 TBstats2012, TBstats2013, TBstats2014, TBstats2015,
+                 TBstats2016, TBstats2017, TBstats2018, TBstats2019,
+                 TBstats2020, TBstats2021)
 
 #Rename columns
 ListTBdf1 <- lapply(ListTBdf, function(x){names(x) <- categories; return(x)})
@@ -78,7 +78,8 @@ TBcareer <- cbind(TBcareer,Superbowl)
 TBcareer$SB_APPER <- ifelse(TBcareer$SB_APPER=="yes",TRUE,FALSE)
 TBcareer$SB_WIN <- ifelse(TBcareer$SB_WIN=="yes",TRUE,FALSE)
 
-#Clean-up white space
+
+#This Line is the Money line
 TBcareer <- TBcareer %>% mutate_if(is.character, str_trim)
 
 #Structure Data
@@ -102,73 +103,3 @@ RATE <- as.numeric(TBcareer$RATE) %>% as.data.frame()
 TBcareer1 <- cbind(G,ATT,COMP,PCT,YDS,AVG,LNG,TD,INT,FIRST,FIRSTPCT,PLUS20,SCK,SCKY,RATE)
 colnames(TBcareer1) <- c("G","ATT","COMP","PCT","YDS","AVG","LNG","TD","INT","FIRST","FIRSTPCT","PLUS20","SCK","SCKY","RATE")
 str(TBcareer1)
-
-
-
-#TDLoop
-
-
-#Regression
-model1 <- lm(TBcareer1$RATE ~ TBcareer1$G )
-
-summary(model1)
-
-
-
-i = as.integer(length(TBcareer$TD))
-bestTD = as.integer(0)
-for(x in 1:i){
-  if(TBcareer$TD[x] > bestTD) {
-    bestTD = TBcareer$TD[x]
-  }
-}
-print (bestTD)
-
-
-
-TBcareer$SCK
-#copy as numeric dataframe
-len = as.integer(nrow(TBcareer))
-
-bradyYear <- 1:len
-bradyG <- 1:len
-bradyATT <- 1:len
-bradyCOMP <- 1:len
-bradyPCT <- 1:len
-bradyYDS <- 1:len
-bradyAVG <- 1:len
-bradyLNG <- 1:len
-bradyTD <- 1:len
-bradyINT <- 1:len
-brady1st <- 1:len
-brady1stPct <- 1:len
-brady20 <- 1:len
-bradySCK <- 1:len
-bradySCKY <- 1:len
-bradyRATE <- 1:len
-
-
-
-
-for(x in 1:len){
-  bradyYear[x] <- as.numeric(TBcareer$YEAR[x])
-  bradyG[x] <- as.numeric(TBcareer$G[x])
-  bradyATT[x] <- as.numeric(TBcareer$ATT[x])
-  bradyCOMP[x] <- as.numeric(TBcareer$COMP[x])
-  bradyPCT[x] <- as.numeric(TBcareer$PCT[x])
-  bradyYDS[x] <- as.numeric(TBcareer$YDS[x])
-  bradyAVG[x] <- as.numeric(TBcareer$AVG[x])
-  bradyLNG[x] <- as.numeric(TBcareer$LNG[x])
-  bradyTD[x] <- as.numeric(TBcareer$TD[x])
-  bradyINT[x] <- as.numeric(TBcareer$INT[x])
-  brady1st[x] <- as.numeric(TBcareer$`1st`[x])
-  brady1stPct[x] <- as.numeric(TBcareer$`1st%`[x])
-  brady20[x] <- as.numeric(TBcareer$`20+`[x])
-  bradySCK[x] <- as.numeric(TBcareer$SCK[x])
-  bradySCKY[x] <- as.numeric(TBcareer$SCKY[x])
-  bradyRATE[x] <- as.numeric(TBcareer$RATE[x])
-}
-
-bradyCareer <- rbind (bradyYear, bradyG, bradyATT, bradyCOMP, bradyPCT, bradyYDS, bradyAVG, bradyLNG, bradyTD, bradyINT,
-                brady1st, brady1stPct, brady20, bradySCK, bradySCKY, bradyRATE)
-
