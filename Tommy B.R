7ead585705fe2461bedd2695e610f6b4b8f56fd0
khoody36 <khood@nevada.unr.edu>
2022-03-22 18:16:16 -0700

    Clean up on aisle 1

--- Tommy B.R
+++ Tommy B.R
@@ -42,33 +42,25 @@ TBstats2019 <- as.data.frame(t(TBstats[c(341:357)]))
 TBstats2020 <- as.data.frame(t(TBstats[c(358:374)]))
 TBstats2021 <- as.data.frame(t(TBstats[c(375:391)]))
 
-#Categories
-categories <- c('YEAR','Team','G','ATT','COMP','PCT',
+#Labels
+xlab <- c('YEAR','Team','G','ATT','COMP','PCT',
                 'YDS','AVG','LNG','TD','INT','1st','1st%',
                 '20+','SCK','SCKY','RATE')
 
-#List of data frames
-ListTBdf <- list(TBstats2000, TBstats2001, TBstats2002, TBstats2003,
-                 TBstats2004, TBstats2005, TBstats2006, TBstats2007,
-                 TBstats2008, TBstats2009, TBstats2010, TBstats2011,
-                 TBstats2012, TBstats2013, TBstats2014, TBstats2015,
-                 TBstats2016, TBstats2017, TBstats2018, TBstats2019,
-                 TBstats2020, TBstats2021)
+ylab <- c('2000','2001','2002','2003','2004','2005','2006',
+          '2007','2008','2009','2010','2011','2012','2013',
+          '2014','2015','2016','2017','2018','2019','2020','2021')
 
-#Rename columns
-ListTBdf1 <- lapply(ListTBdf, function(x){names(x) <- categories; return(x)})
-
-#Combine data frames
+#Create career data
 TBcareer <- rbind(TBstats2000, TBstats2001, TBstats2002, TBstats2003,
                   TBstats2004, TBstats2005, TBstats2006, TBstats2007,
                   TBstats2008, TBstats2009, TBstats2010, TBstats2011,
                   TBstats2012, TBstats2013, TBstats2014, TBstats2015,
                   TBstats2016, TBstats2017, TBstats2018, TBstats2019,
                   TBstats2020, TBstats2021)
-
-#Rename columns TBcareer
-colnames(TBcareer) <- categories
-
+colnames(TBcareer) <- xlab
+rownames(TBcareer) <- ylab
+TBcareer <- subset(TBcareer, select= -YEAR)
 
 #Superbowl Outcome
 Superbowl <- data.frame(SB_APPER = c("no","yes","no","yes","yes","no","no","yes","no","no","no","yes","no","no","yes","no","yes","yes","yes","no","yes","no"),
@@ -104,6 +96,7 @@ SB_APPER <- as.numeric(TBcareer$SB_APPER) %>% as.data.frame()
 
 TBcareer1 <- cbind(G,ATT,COMP,PCT,YDS,AVG,LNG,TD,INT,FIRST,FIRSTPCT,PLUS20,SCK,SCKY,RATE,SB_APPER)
 colnames(TBcareer1) <- c("G","ATT","COMP","PCT","YDS","AVG","LNG","TD","INT","FIRST","FIRSTPCT","PLUS20","SCK","SCKY","RATE","SB_APPER")
+rownames(TBcareer1) <- ylab
 str(TBcareer1)
 
 #Regression
