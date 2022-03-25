874a4fe9d710af2d52f76d87d6d3a0dcf6a3143a
khoody36 <khood@nevada.unr.edu>
2022-03-24 18:54:07 -0700

    scRAPIST

---
+++ Mess around and findout.R
@@ -0,0 +1,128 @@
+library(rvest)
+library(xml2)
+library(dplyr)
+library(stringr)
+library(ggplot2)
+library(plotly)
+library(robotstxt)
+library(tidyr)
+library(caTools)
+
+#check permissions
+paths_allowed("https://www.nfl.com/players/tom-brady/stats/career")
+
+#create link for Tom Brady career stats
+link <- "https://www.nfl.com/players/tom-brady/stats/career"
+
+#Pull stats 
+TBstats <- read_html("https://www.nfl.com/players/tom-brady/stats/career") %>% 
+  html_nodes("td.nfl-t-stats__col-16") %>% 
+  html_text(trim = T) %>% 
+  str_squish()
+
+StatCol <- read_html("https://www.nfl.com/players/tom-brady/stats/career") %>% 
+  html_nodes("th.nfl-t-stats__col-16") %>% 
+  html_text(trim = T) %>% 
+  str_squish()
+
+TB <- as.data.frame(t(TBstats))
+Columns <- as.data.frame(t(StatCol))
+str(TB)
+
+TB2 <- pivot_longer(TB, c(V1,V18))
+
+#Make stats by year
+StatsCat <- as.data.frame(TBstats[c(1:17)])
+TBstats2000 <- as.data.frame(t(TBstats[c(18:34)]))
+TBstats2001 <- as.data.frame(t(TBstats[c(35:51)]))
+TBstats2002 <- as.data.frame(t(TBstats[c(52:68)]))
+TBstats2003 <- as.data.frame(t(TBstats[c(69:85)]))
+TBstats2004 <- as.data.frame(t(TBstats[c(86:102)]))
+TBstats2005 <- as.data.frame(t(TBstats[c(103:119)]))
+TBstats2006 <- as.data.frame(t(TBstats[c(120:136)]))
+TBstats2007 <- as.data.frame(t(TBstats[c(137:153)]))
+TBstats2008 <- as.data.frame(t(TBstats[c(154:170)]))
+TBstats2009 <- as.data.frame(t(TBstats[c(171:187)]))
+TBstats2010 <- as.data.frame(t(TBstats[c(188:204)]))
+TBstats2011 <- as.data.frame(t(TBstats[c(205:221)]))
+TBstats2012 <- as.data.frame(t(TBstats[c(222:238)]))
+TBstats2013 <- as.data.frame(t(TBstats[c(239:255)]))
+TBstats2014 <- as.data.frame(t(TBstats[c(256:272)]))
+TBstats2015 <- as.data.frame(t(TBstats[c(273:289)]))
+TBstats2016 <- as.data.frame(t(TBstats[c(290:306)]))
+TBstats2017 <- as.data.frame(t(TBstats[c(307:323)]))
+TBstats2018 <- as.data.frame(t(TBstats[c(324:340)]))
+TBstats2019 <- as.data.frame(t(TBstats[c(341:357)]))
+TBstats2020 <- as.data.frame(t(TBstats[c(358:374)]))
+TBstats2021 <- as.data.frame(t(TBstats[c(375:391)]))
+
+#Labels
+xlab <- c('YEAR','TEAM','G','ATT','COMP','PCT',
+          'YDS','AVG','LNG','TD','INT','1st','1st%',
+          '20+','SCK','SCKY','RATE')
+
+ylab <- c('2000','2001','2002','2003','2004','2005','2006',
+          '2007','2008','2009','2010','2011','2012','2013',
+          '2014','2015','2016','2017','2018','2019','2020','2021')
+
+#Create career data
+TBcareer <- rbind(TBstats2000, TBstats2001, TBstats2002, TBstats2003,
+                  TBstats2004, TBstats2005, TBstats2006, TBstats2007,
+                  TBstats2008, TBstats2009, TBstats2010, TBstats2011,
+                  TBstats2012, TBstats2013, TBstats2014, TBstats2015,
+                  TBstats2016, TBstats2017, TBstats2018, TBstats2019,
+                  TBstats2020, TBstats2021)
+colnames(TBcareer) <- xlab
+TBcareer <- TBcareer %>% mutate_if(is.character, str_trim)
+
+#Superbowl Outcome
+Superbowl <- data.frame(SB_APPER = c("no","yes","no","yes","yes","no","no","yes","no","no","no","yes","no","no","yes","no","yes","yes","yes","no","yes","no"),
+                        SB_WIN = c("no","yes","no","yes","yes","no","no","no","no","no","no","no","no","no","yes","no","yes","no","yes","no","yes","no"))
+
+#Add Superbowl data to TBcareer
+TBcareer <- cbind(TBcareer,Superbowl)
+TBcareer$SB_APPER <- ifelse(TBcareer$SB_APPER=="yes",1,0)
+TBcareer$SB_WIN <- ifelse(TBcareer$SB_WIN=="yes",1,0)
+
+#TBcareer structure
+str(TBcareer)
+
+#Structure Data
+G <- as.numeric(TBcareer$G) %>% as.data.frame()
+ATT <- as.numeric(TBcareer$ATT) %>% as.data.frame()
+COMP <- as.numeric(TBcareer$COMP) %>% as.data.frame()
+PCT <- as.numeric(TBcareer$PCT) %>% as.data.frame()
+YDS <- as.numeric(TBcareer$YDS) %>% as.data.frame()
+AVG <- as.numeric(TBcareer$AVG) %>% as.data.frame()
+LNG <- as.numeric(TBcareer$LNG) %>% as.data.frame()
+TD <- as.numeric(TBcareer$TD) %>% as.data.frame()
+INT <- as.numeric(TBcareer$INT) %>% as.data.frame()
+FIRST <- as.numeric(TBcareer$`1st`) %>% as.data.frame()
+FIRSTPCT <- as.numeric(TBcareer$`1st%`) %>% as.data.frame()
+PLUS20 <- as.numeric(TBcareer$`20+`) %>% as.data.frame()
+SCK <- as.numeric(TBcareer$SCK) %>% as.data.frame()
+SCKY <- as.numeric(TBcareer$SCKY)%>% as.data.frame()
+RATE <- as.numeric(TBcareer$RATE) %>% as.data.frame()
+SB_APPER <- as.numeric(TBcareer$SB_APPER) %>% as.data.frame()
+
+TBcareer1 <- cbind(TBcareer$YEAR,TBcareer$TEAM,G,ATT,COMP,PCT,YDS,AVG,LNG,TD,INT,FIRST,FIRSTPCT,PLUS20,SCK,SCKY,RATE,SB_APPER)
+colnames(TBcareer1) <- c("YEAR","TEAM","G","ATT","COMP","PCT","YDS","AVG","LNG","TD","INT","FIRST","FIRSTPCT","PLUS20","SCK","SCKY","RATE","SB_APPER")
+str(TBcareer1)
+
+#Regression
+TBcareer2 <- TBcareer1[!(TBcareer1$YEAR %in% c(2000,2008)), ]
+
+split <- sample.split(TBcareer2, SplitRatio = 0.5)
+training <- subset(TBcareer2, split=="TRUE")
+testing <- subset(TBcareer2, split=="FALSE")
+
+model1 <- glm(SB_APPER ~ RATE + TD + PCT - INT, training, family = "binomial")
+summary(model1)
+
+data <- data.frame(RATE=92.6, TD=28, PCT=60.76, INT=14)
+data2 <- data.frame(RATE=102.1,TD=43, PCT=67.45, INT=12)
+
+answer <- predict.glm(model1,data,type = "response")>.5
+answer2 <- predict.glm(model1,data2,type = "response")>.5
+
+print(c(answer,answer2))
