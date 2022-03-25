b8f71f060fc855c4572e2eeb0c1b2c5bccf9d027
Kyle Hood <kehood@detr.nv>
2022-03-25 09:53:30 -0700

    scRAPIST 4

--- Mess around and findout.R
+++ Mess around and findout.R
@@ -15,7 +15,7 @@ paths_allowed("https://www.nfl.com/players/tom-brady/stats/career")
 link <- "https://www.nfl.com/players/tom-brady/stats/career"
 
 #Pull stats 
-TB <- read_html("https://www.nfl.com/players/tom-brady/stats/career") %>% 
+TB <- read_html(link) %>% 
   html_nodes(".nfl-o-roster") %>% 
   html_table()
 
@@ -33,25 +33,20 @@ TBcareer$SB_WIN <- ifelse(TBcareer$SB_WIN=="yes",1,0)
 #TBcareer structure
 str(TBcareer)
 
-
-TBcareer1 <- cbind(TBcareer$YEAR,TBcareer$TEAM,G,ATT,COMP,PCT,YDS,AVG,LNG,TD,INT,FIRST,FIRSTPCT,PLUS20,SCK,SCKY,RATE,SB_APPER)
-colnames(TBcareer1) <- c("YEAR","TEAM","G","ATT","COMP","PCT","YDS","AVG","LNG","TD","INT","FIRST","FIRSTPCT","PLUS20","SCK","SCKY","RATE","SB_APPER")
-str(TBcareer1)
-
 #Regression
-TBcareer2 <- TBcareer1[!(TBcareer1$YEAR %in% c(2000,2008)), ]
+TBcareer1 <- TBcareer[!(TBcareer$YEAR %in% c(2000,2008)), ]
 
-split <- sample.split(TBcareer2, SplitRatio = 0.5)
-training <- subset(TBcareer2, split=="TRUE")
-testing <- subset(TBcareer2, split=="FALSE")
+split <- sample.split(TBcareer1, SplitRatio = 0.5)
+training <- subset(TBcareer1, split=="TRUE")
+testing <- subset(TBcareer1, split=="FALSE")
 
 model1 <- glm(SB_APPER ~ RATE + TD + PCT - INT, training, family = "binomial")
 summary(model1)
 
-data <- data.frame(RATE=92.6, TD=28, PCT=60.76, INT=14)
-data2 <- data.frame(RATE=102.1,TD=43, PCT=67.45, INT=12)
+data2004 <- data.frame(RATE=92.6, TD=28, PCT=60.76, INT=14)
+data2021 <- data.frame(RATE=102.1,TD=43, PCT=67.45, INT=12)
 
-answer <- predict.glm(model1,data,type = "response")>.5
-answer2 <- predict.glm(model1,data2,type = "response")>.5
+answer <- predict.glm(model1,data2004,type = "response")>.5
+answer2 <- predict.glm(model1,data2021,type = "response")>.5
 
 print(c(answer,answer2))
